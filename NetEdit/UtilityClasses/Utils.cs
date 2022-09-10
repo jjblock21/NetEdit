4c5a626bcc299cfb133a4ca412a69b5d06ea26c7
jjblock21 <jjblock21@gmail.com>
2022-09-10 17:46:04 +0200

    Correctly implemented filtering the fileExtension string.

--- NetEdit/UtilityClasses/Utils.cs
+++ NetEdit/UtilityClasses/Utils.cs
@@ -16,5 +16,37 @@ namespace NetEdit.UtilityClasses
         {
             MessageBox.Show(message, "Uncaught Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
+
+        #region Extensions
+
+        //TODO: Make coments for this
+        public static string Filter(this string str, params char[] filterChars)
+        {
+            StringBuilder b = new StringBuilder();
+            int i = 0;
+            foreach (char c in str)
+            {
+                if (filterChars.Contains(c))
+                    continue;
+                b.Append(c);
+                i++;
+            }
+            return b.ToString();
+        }
+
+        public static string Filter(this string str, char filterChar)
+        {
+            StringBuilder b = new StringBuilder();
+            int i = 0;
+            foreach (char c in str)
+            {
+                if (c == filterChar) continue;
+                b.Append(c);
+                i++;
+            }
+            return b.ToString();
+        }
+
+        #endregion
     }
 }
