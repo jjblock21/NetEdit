119c9f9c40a435df6efbb7bfc1d3c51086a5a76d
jjblock21 <jjblock21@gmail.com>
2022-09-11 19:37:03 +0200

    Added ColorScheme colors for tool strip.

--- Dodecaplex.DarkGui/ColorScheme.cs
+++ Dodecaplex.DarkGui/ColorScheme.cs
@@ -15,32 +15,53 @@
         public Color textColor;
         public Color backColor;
 
+
         //Button related control colors
         public Color ctrl_backColor;
         public Color ctrl_borderColor;
+
         public Color ctrl_clicked_backColor;
         public Color ctrl_clicked_borderColor;
+
         public Color ctrl_disabled_backColor;
         public Color ctrl_disabled_borderColor;
+
         public Color ctrl_hover_backColor;
         public Color ctrl_hover_borderColor;
         public Color ctrl_hover_textColor;
 
-        //Main tool strip colors.
+
+        //Menu strip colors.
         public Color menu_dropdown_borderColor;
         public Color menu_dropdown_backColor;
+
         public Color menu_backColor;
         public Color menu_textColor;
+
         public Color menu_button_backColor;
         public Color menu_hover_backColor;
         public Color menu_hover_borderColor;
         public Color menu_disabled_textColor;
         public Color menu_disabled_backColor;
+
         public Color menu_seperator;
         public Color menu_imageMargin;
 
+
+        //Tool strip colors
+        public Color tools_backColor;
+        public Color tools_textColor;
+        public Color tools_borderColor;
+
+        public Color tools_hover_backColor;
+        public Color tools_hover_borderColor;
+
+        public Color tools_disabled_textColor;
+        public Color tools_disabled_backColor;
+
+
         #region ControlProperties
-        //Use custom renderer for the tool strip.
+        //Use custom renderer for tool strips.
         public bool menu_useCustomRenderer;
         #endregion
 
